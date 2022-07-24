c546fbe74ef05dede74a86a5b6c04ee0cf80020e
superjcd <929760274@qq.com>
2022-07-24 12:27:51 +0800

    finshi sidebar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,19 @@
 import "./home.scss"
-
+import Sidebar from "../../components/sidebar/Sidebar" 
 
 const home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+       <Sidebar />
+       <div className="homeContainer">
+            <div>
+                navbar
+            </div>
+            <div>
+                main
+            </div>
+        </div> 
+    </div>
   )
 }
 
